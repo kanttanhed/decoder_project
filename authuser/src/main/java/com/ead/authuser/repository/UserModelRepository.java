fe18879747da5c98f323826bc93f577e63485eec
Plinio <plinio.salazar@yahoo.com.br>
2024-01-29 19:01:03 +0000

    feat: Creating initial structure and the UserModel entitie

---
+++ authuser/src/main/java/com/ead/authuser/repository/UserModelRepository.java
@@ -0,0 +1,9 @@
+package com.ead.authuser.repository;
+
+import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserModelRepository extends JpaRepository<UserModel, UUID> {
+}
