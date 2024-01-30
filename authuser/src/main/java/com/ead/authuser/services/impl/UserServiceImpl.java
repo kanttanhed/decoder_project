861cd8a4916e2039bf939ffea2b17736e97a8078
Plinio <plinio.salazar@yahoo.com.br>
2024-01-30 21:48:47 +0000

    feat: Created User Controller

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,9 +1,17 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repository.UserModelRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,4 +19,18 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserModelRepository userModelRepository;
 
+    @Override
+    public List<UserModel> findAll() {
+        return userModelRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userModelRepository.findById(userId);
+    }
+
+    @Override
+    public void deleteUser(UserModel userModel) {
+        userModelRepository.delete(userModel);
+    }
 }
